debacc67df365c7b267551bf0c69585a76fac4f8
Suraj <surajsinghrawat12345@gmail.com>
2017-05-15 15:52:38 +0530

    [SURAJ] initial Commit to set up project

---
+++ Serialization/src/main/java/com/sadp/app/MainApplication.java
@@ -0,0 +1,11 @@
+package com.sadp.app;
+
+/**
+ * Created by srawa5 on 5/15/2017.
+ */
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Java Serialization");
+    }
+}
